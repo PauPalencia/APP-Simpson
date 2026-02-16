321db96459fd4a6f656f53a3bbee4b3ffdbb2451
PauPalencia <ppalencia@alum.joviat.cat>
2026-02-16 12:54:52 +0100

    Add favorite watched and play controls to episode detail

--- src/screens/SeasionScreen.js
+++ src/screens/SeasionScreen.js
@@ -3,12 +3,32 @@ import { View, FlatList, Button, StyleSheet } from "react-native";
 import Card from "../components/card";
 import { episodeImages } from "../utils/episodeImages";
 import seasons from "../components/Episodes/seasons.json";
+import infoCaps from "../components/Episodes/Info.Caps";
+
+const episodeInfoById = new Map(
+  infoCaps.seasons.flatMap((season) =>
+    season.episodes.map((episode) => [episode.id, episode])
+  )
+);
 
 export default function SeasonScreen({ route, navigation }) {
   const { seasonKey } = route.params;
   const [isList, setIsList] = useState(false);
 
-  const episodes = seasons[seasonKey] || [];
+  const episodes = (seasons[seasonKey] || []).map((episode) => {
+    const episodeId = episode.image.replace(".webp", "");
+    const extraInfo = episodeInfoById.get(episodeId);
+
+    if (!extraInfo) {
+      return episode;
+    }
+
+    return {
+      ...episode,
+      title: extraInfo.title,
+      description: extraInfo.synopsis,
+    };
+  });
 
   const listKey = isList ? 'list' : 'grid';
 
